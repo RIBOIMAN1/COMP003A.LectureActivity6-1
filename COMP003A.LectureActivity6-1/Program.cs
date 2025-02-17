0721a3d730a2488b87ce7c68932cd90688c34d37
RIBOIMAN1 <rileybenson@my.whccd.edu>
2025-02-17 01:00:16 -0800

    Added code from step 2 of the 6.2 lecture activity.

--- COMP003A.LectureActivity6-1/Program.cs
+++ COMP003A.LectureActivity6-1/Program.cs
@@ -2,7 +2,32 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+		/// <summary>
+		/// Represents a general vehicle.
+		/// </summary>
+		class Vehicle
+		{
+			// Fields
+			private string _brand;
+
+			// Properties
+			public string Brand
+			{
+				get { return _brand; }
+				set { _brand = value; }
+			}
+
+			public int Year { get; set; }
+
+			/// <summary>
+			/// Displays vehicle details.
+			/// </summary>
+			public virtual void DisplayInfo()
+			{
+				Console.WriteLine($"Brand: {Brand}, Year: {Year}");
+			}
+		}
+		static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
         }
